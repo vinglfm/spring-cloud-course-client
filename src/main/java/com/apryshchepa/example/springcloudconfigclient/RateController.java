5af1d0e5d0add22c968d4238c036f9517bc654e9
Andrew Pryshchepa <vinglfm@gmail.com>
2018-08-25 17:24:55 +0300

    Cloud config client

---
+++ src/main/java/com/apryshchepa/example/springcloudconfigclient/RateController.java
@@ -0,0 +1,31 @@
+package com.apryshchepa.example.springcloudconfigclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RefreshScope
+@Controller
+public class RateController {
+
+    @Value("${rate}")
+    private String rate;
+
+    @Value("${lanecount}")
+    private String lanecount;
+
+    @Value("${tollstart}")
+    private String tollstart;
+
+    @RequestMapping("/rate")
+    public String getRate(Model model) {
+
+        model.addAttribute("rateamount", rate);
+        model.addAttribute("lanes", lanecount);
+        model.addAttribute("tollstart", tollstart);
+
+        return "rates";
+    }
+}
